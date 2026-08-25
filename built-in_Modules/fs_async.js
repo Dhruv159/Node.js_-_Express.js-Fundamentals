0f8b02758865f0d50675b293e678c60b6edb407b
Dhruv Sharma <dhruvsarkhandia9@gmail.com>
2026-08-25 20:11:20 +0530

    Async fs built-in modules syntax example

---
+++ built-in_Modules/fs_async.js
@@ -0,0 +1,26 @@
+const { readFile, writeFile } = require('fs');
+
+readFile('../content/first.txt', 'utf8',(err, data) => {
+    if(err) return;
+    const first = data;
+    console.log('Result1 :-', data);
+
+    readFile('../content/second.txt', 'utf8', (err, data) => {
+        if(err) return;
+        const second = data;
+        console.log('Result2 :-', data)
+
+        writeFile(
+            '../content/result-async.txt',
+            `Here is the result ::-- ${first}, ${second}`,
+            (err, data) => {
+                if(err) {
+                    console.log('WriteFile Error:', err);
+                    return;
+                }
+                console.log('WriteFile Result: ', data)
+            }
+
+        )
+    });
+});
